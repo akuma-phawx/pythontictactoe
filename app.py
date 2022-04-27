7dffa3e39a3a1ab730631b818e30b3cc052be26f
Christoforos Vradis <chrisvradis00@gmail.com>
2022-04-27 22:37:12 +0300

    feat(endgame): add endgame content

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 table = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
 available_cells = list(range(0, 9))
-winning_cells = ([0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-                 [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6])
 
 players = {'1': {'name': '', 'symbol': 'X'}, '2': {'name': '', 'symbol': 'O'}}
 
@@ -31,7 +29,7 @@ def introduction():
     players['1']['name'] = input('\t Player 1 name: ')
     players['2']['name'] = input('\t Player 2 name: ')
     print(
-        f"\nPlayer One: {players['1']['name']} with (X) vs Player Two: {players['2']['name']} with (O).. Let the game begin!\n")
+        f"\nPlayer One: {players['1']['name']} with (X)\nvs\nPlayer Two: {players['2']['name']} with (O)\n.. Let the game begin!\n")
 
 
 def start_game():
@@ -39,13 +37,15 @@ def start_game():
     print_tic_tac_toe()
 
     while not gameOver:
-        cell = int(
-            input(f"\n{players[str(playerPlaying)]['name']} pick a cell: "))
-        while cell not in available_cells:
-            cell = int(input(
-                f"Cell {cell} is not available. Player {players[str(playerPlaying)]['name']} pick a cell: "))
+        cell = input(f"\n{players[str(playerPlaying)]['name']} pick a cell: ")
+        while not cell.strip().isdigit() or int(cell) not in available_cells:
+            cell = input(
+                f"Woops, wrong input or cell is taken. Player {players[str(playerPlaying)]['name']} try again: ")
 
+        cell = int(cell)
         available_cells.remove(cell)
+
+        global table
         table[cell] = players[str(playerPlaying)]['symbol']
         print_tic_tac_toe()
         check_if_winner()
@@ -53,7 +53,18 @@ def start_game():
 
 
 def check_if_winner():
-    pass
+    global gameOver
+    winning_combinations = ([table[0], table[1], table[2]], [table[3], table[4], table[5]], [table[6], table[7], table[8]], [table[0], table[3], table[6]],
+                            [table[1], table[4], table[7]], [table[2], table[5], table[8]], [table[0], table[4], table[8]], [table[2], table[4], table[6]])
+
+    for combo in winning_combinations:
+        if len(set(combo)) == 1:
+            gameOver = True
+            print('\nGame Over!')
+            if combo[0] == 'X':
+                print(f"{players['1']['name']} wins!")
+            else:
+                print(f"{players['2']['name']} wins!")
 
 
 def change_player():
